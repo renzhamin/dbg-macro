a5d927e729c11d6cb9570c02ffa6c002f6755bc4
renzhamin <tasnimferdous9@iut-dhaka.edu>
2022-09-18 20:01:06 +0600

    FILE_PATH removed, support for printing indices

--- dbg.h
+++ dbg.h
@@ -3,28 +3,13 @@
 #define DBG_MACRO_DBG_H
 
 // #define DBG_SHOW_TYPE_NAME
-// #define DBG_SHOW_FILE_PATH
-#define DBG_MAX_ELEMS 15
+// #define DBG_SHOW_PREFIX
+// #define DBG_MAX_ELEMS 25
 
-#ifdef DBG_SHOW_FILE_PATH
-#define FILE_PATH __FILE__
-#else
-#define FILE_PATH ""
-#endif
 
-#ifdef DBG_SHOW_TYPE_NAME
-#define PRINT_TYPE_NAME(x) cerr<<"( "<<DBG_TYPE_NAME(x)<<" )";
-#else
-#define PRINT_TYPE_NAME(x)
-#endif
-
-#define sdbg(x,i,j)\
-    cerr<< DBG_STRINGIFY(x)<<" = ";dbg::pretty_print(cerr,x,i,j); \
-    PRINT_TYPE_NAME(x) \
-    cerr<<'\n';
 
 #ifndef DBG_MAX_ELEMS
-#define DBG_MAX_ELEMS 10
+#define DBG_MAX_ELEMS 15
 #endif
 
 #if defined(__unix__) || (defined(__APPLE__) && defined(__MACH__))
@@ -428,7 +413,7 @@ inline bool pretty_print(std::ostream& stream,
 template <typename Container>
 inline typename std::enable_if<detail::is_container<const Container&>::value,
                                bool>::type
-pretty_print(std::ostream& stream, const Container& value, size_t first=0, size_t last=0);
+pretty_print(std::ostream& stream, const Container& value, size_t first=0, size_t last=0, bool printInd=0);
 
 // Specializations of "pretty_print"
 
@@ -698,7 +683,7 @@ inline bool pretty_print(std::ostream& stream,
 template <typename Container>
 inline typename std::enable_if<detail::is_container<const Container&>::value,
                                bool>::type
-pretty_print(std::ostream& stream, const Container& value, size_t first, size_t last) {
+pretty_print(std::ostream& stream, const Container& value, size_t first, size_t last, bool printInd) {
   stream << "[ ";
   const size_t size = detail::size(value);
   size_t n = std::min(size_t{DBG_MAX_ELEMS}, size);
@@ -707,6 +692,7 @@ pretty_print(std::ostream& stream, const Container& value, size_t first, size_t 
   using std::begin;
   using std::end;
   for (auto it = begin(value)+i; it != end(value) && i < n; ++it, ++i) {
+    if(printInd) stream << i << ":";
     pretty_print(stream, *it);
     if (i != n - 1) {
       stream << ", ";
@@ -744,19 +730,16 @@ class DebugOutput {
   // Helper alias to avoid obscure type `const char* const*` in signature.
   using expr_t = const char*;
   bool prefix = 1;
-  bool newline = 1;
+  bool sideByside = 1;
 
-  DebugOutput(const char* filepath, int line, const char* function_name, bool newline=1, bool prefix=0)
-      : m_use_colorized_output(isColorizedOutputEnabled()), prefix(prefix),newline(newline) {
-          std::string path = filepath;
-    const std::size_t path_length = path.length();
-    if (path_length > MAX_PATH_LENGTH) {
-      path = ".." + path.substr(path_length - MAX_PATH_LENGTH, MAX_PATH_LENGTH);
-    }
+  DebugOutput(int line, const char* function_name, bool sideByside=0)
+      : m_use_colorized_output(isColorizedOutputEnabled()), sideByside(sideByside) {
     std::stringstream ss;
-    if(prefix && newline)
-        ss << ansi(ANSI_DEBUG) << "[" << path << ":" << line << " ("
+#ifdef DBG_SHOW_PREFIX
+    if(!sideByside)
+        ss << ansi(ANSI_DEBUG) << "[" << line << " ("
        << function_name << ")] " << ansi(ANSI_RESET);
+#endif
     m_location = ss.str();
   }
 
@@ -782,7 +765,7 @@ class DebugOutput {
 
     std::stringstream output;
     output << m_location;
-    if(!newline)
+    if(sideByside)
         output << "[ ";
     if (print_expr_and_type) {
       output << ansi(ANSI_EXPRESSION) << *expr << ansi(ANSI_RESET) << " = ";
@@ -790,11 +773,11 @@ class DebugOutput {
     output << ansi(ANSI_VALUE) << stream_value.str() << ansi(ANSI_RESET);
 
 #ifdef DBG_SHOW_TYPE_NAME
-    if (print_expr_and_type) {
+    if (!sideByside && print_expr_and_type) {
       output << " (" << ansi(ANSI_TYPE) << *type << ansi(ANSI_RESET) << ")";
     }
 #endif
-    if(newline)
+    if(!sideByside)
         output << std::endl;
     else
         output << " ]  ";
@@ -905,15 +888,33 @@ auto identity(T&&, U&&... u) -> last_t<U...> {
 #define DBG_TYPE_NAME(x) dbg::type_name<decltype(x)>()
 
 #define dbg(...)                                    \
-  dbg::DebugOutput(FILE_PATH, __LINE__, __func__)    \
+  dbg::DebugOutput(__LINE__, __func__)    \
       .print({DBG_MAP(DBG_STRINGIFY, __VA_ARGS__)}, \
              {DBG_MAP(DBG_TYPE_NAME, __VA_ARGS__)}, __VA_ARGS__)
 
 #define dbs(...)                                    \
-  dbg::DebugOutput(FILE_PATH, __LINE__, __func__, 0, 0)    \
+  dbg::DebugOutput(__LINE__, __func__, 1)    \
       .print({DBG_MAP(DBG_STRINGIFY, __VA_ARGS__)}, \
              {DBG_MAP(DBG_TYPE_NAME, __VA_ARGS__)}, __VA_ARGS__); \
              cerr << '\n'
+
+
+
+
+// ----------------------------dbr(arr, 1, n, printInd=0)----------------------------------------------
+#ifdef DBG_SHOW_TYPE_NAME
+#define PRINT_TYPE_NAME(x) cerr<<"( "<<DBG_TYPE_NAME(x)<<" )";
+#else
+#define PRINT_TYPE_NAME(x)
+#endif
+
+#define dbr(...)\
+    cerr<< DBG_STRINGIFY(DBG_HEAD_IMPL(__VA_ARGS__))<<" = ";\
+    dbg::pretty_print(cerr,__VA_ARGS__); \
+    PRINT_TYPE_NAME(x) \
+    cerr<<'\n';
+// ----------------------------------------------------------------------------------------------------
+
 
 #else
 #define dbg(...) dbg::identity(__VA_ARGS__)
